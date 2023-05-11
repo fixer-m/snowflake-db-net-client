59548704a24cb3f5594b637c56583654312e0af4
colgreen <colin.green1@gmail.com>
2023-05-11 22:04:25 +0100

    Added HexUtilsTests.

--- Snowflake.Client.Tests/UnitTests/HexUtilsTests.cs
+++ Snowflake.Client.Tests/UnitTests/HexUtilsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Text;
 using NUnit.Framework;
 using Snowflake.Client.Helpers;
@@ -21,5 +22,31 @@ namespace Snowflake.Client.Tests.UnitTests
 
             Assert.AreEqual(expectedBase64, sb.ToString());
         }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public void ConvertHexToBytes_VaryingLengths(string hex, byte[] expectedBytes)
+        {
+            string expectedBase64 = Convert.ToBase64String(expectedBytes);
+
+            var sb = new StringBuilder();
+            HexUtils.HexToBase64(hex, sb);
+
+            Assert.AreEqual(expectedBase64, sb.ToString());
+        }
+
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("00", new byte[] { 0x00 });
+                yield return new TestCaseData("0080", new byte[] { 0x00, 0x80 });
+                yield return new TestCaseData("0080ff", new byte[] { 0x00, 0x80, 0xff });
+                yield return new TestCaseData("0a0b0c0d", new byte[] { 0x0a, 0x0b, 0x0c, 0x0d });
+                yield return new TestCaseData("0a0b0c0d0e", new byte[] { 0x0a, 0x0b, 0x0c, 0x0d, 0x0e });
+                yield return new TestCaseData("0a0b0c0d0e0f", new byte[] { 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f });
+            }
+        }
+
     }
 }

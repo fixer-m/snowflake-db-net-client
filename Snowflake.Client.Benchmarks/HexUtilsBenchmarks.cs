1e66a7ef4757c839be364627252c4eaa94d0fdfb
colgreen <colin.green1@gmail.com>
2023-05-11 22:04:54 +0100

    Added Snowflake.Client.Benchmarks project.

---
+++ Snowflake.Client.Benchmarks/HexUtilsBenchmarks.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using Snowflake.Client.Helpers;
+
+namespace Snowflake.Client.Benchmarks;
+
+[MemoryDiagnoser]
+public class HexUtilsBenchmarks
+{
+    static readonly string __hexCharsLong = GenerateRandomHex(2_000_000);
+
+    [Benchmark]
+    public void HexToBytes_Short()
+    {
+        var sb = new StringBuilder();
+        HexUtils.HexToBase64("0a0b0c", sb);
+    }
+
+    [Benchmark]
+    public void HexToBytes_Long()
+    {
+        var sb = new StringBuilder();
+        HexUtils.HexToBase64(__hexCharsLong, sb);
+    }
+
+    private static string GenerateRandomHex(int length)
+    {
+        Random rng = new(123);
+
+        StringBuilder sb = new(length);
+        for(int i = 0; i < length; i++)
+        {
+            sb.Append(rng.Next(0, 16).ToString("X"));
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Snowflake.Client.Benchmarks/SnowflakeDataMapperBenchmarks.cs
@@ -0,0 +1,101 @@
+﻿using BenchmarkDotNet.Attributes;
+using Snowflake.Client.Json;
+
+namespace Snowflake.Client.Benchmarks;
+
+[MemoryDiagnoser]
+public class SnowflakeDataMapperBenchmarks
+{
+    readonly QueryExecResponseData _responseSample;
+    readonly List<List<string>> _objectRowset;
+
+    public SnowflakeDataMapperBenchmarks()
+    {
+        _responseSample = GetFakeResponse();
+        _objectRowset = new List<List<string>>() { _responseSample.RowSet[0] };
+    }
+
+    [Benchmark]
+    public void ResponseWithValues_MapTo_CustomClass()
+    {
+        SnowflakeDataMapper.MapTo<CustomClass>(_responseSample.RowType, _objectRowset);
+    }
+
+    private static QueryExecResponseData GetFakeResponse()
+    {
+        var response = new QueryExecResponseData() { RowType = new List<ColumnDescription>(), RowSet = new List<List<string>>() };
+
+        response.RowType.Add(new ColumnDescription() { Name = "StringProperty", Type = "text" });
+        response.RowType.Add(new ColumnDescription() { Name = "BoolProperty", Type = "boolean" });
+        response.RowType.Add(new ColumnDescription() { Name = "IntProperty", Type = "fixed" });
+        response.RowType.Add(new ColumnDescription() { Name = "FloatProperty", Type = "real" });
+        response.RowType.Add(new ColumnDescription() { Name = "DecimalProperty", Type = "real" });
+        response.RowType.Add(new ColumnDescription() { Name = "DateTimeProperty", Type = "timestamp_ntz" });
+        response.RowType.Add(new ColumnDescription() { Name = "DateTimeOffsetProperty", Type = "timestamp_ltz" });
+        response.RowType.Add(new ColumnDescription() { Name = "GuidProperty", Type = "text" });
+        response.RowType.Add(new ColumnDescription() { Name = "ByteArrayProperty", Type = "binary" });
+
+        response.RowSet.Add(new List<string>()
+            {
+                "Sometext",
+                "true",
+                "7",
+                "27.6",
+                "19.239834",
+                "1600000000.000000000",
+                "1600000000.000000000",
+                "e7412bbf-88ee-4149-b341-101e0f72ec7c",
+                "0080ff"
+            });
+
+        response.RowSet.Add(new List<string>()
+            {
+                "null",
+                "null",
+                "null",
+                "null",
+                "null",
+                "null",
+                "null",
+                "null",
+                "null"
+            });
+
+
+        response.RowSet.Add(new List<string>()
+            {
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+            });
+
+        return response;
+    }
+
+    private class CustomClass
+    {
+        public string StringProperty { get; set; }
+
+        public bool BoolProperty { get; set; }
+
+        public int IntProperty { get; set; }
+
+        public float FloatProperty { get; set; }
+
+        public decimal DecimalProperty { get; set; }
+
+        public DateTime DateTimeProperty { get; set; }
+
+        public DateTimeOffset DateTimeOffsetProperty { get; set; }
+
+        public Guid GuidProperty { get; set; }
+
+        public byte[] ByteArrayProperty { get; set; }
+    }
+}
